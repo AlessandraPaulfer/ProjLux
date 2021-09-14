258e43ff7170d87ef2726442c2dc7b8ea46ba054
Alessandra Fernandes <alessandrafnds@gmail.com>
2021-09-14 12:02:33 -0300

    Primeiro commit

---
+++ Model/Time.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjLux.Model
+{
+    public class Time
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Data { get; set; }
+        List<Jogador> Jogadores { get; set; }
+        public Time (string nome)
+        {
+            Nome = nome;
+            Jogadores = new List<Jogador>();
+        }
+    }
+}
